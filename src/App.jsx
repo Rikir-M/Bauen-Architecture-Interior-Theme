8f1bc83f4b6699b7cd912f3eaaefaf0950bdae3c
NiNiShyuneLae <heathersan865@gmail.com>
2023-06-18 21:14:09 +0630

    blog finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // import React from 'react'
+import BlogCard from './components/BlogCard'
+import BlogDetail from './components/BlogDetail'
 import Navbar from './components/Navbar'
+import Blog from './pages/Blog'
 import Home from './pages/Home'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className=''>
-      <Navbar/>
-      <Home/>
+    <div>
+      <Routes>
+        <Route path='/' element={<Blog/>}/>
+        <Route path='/blogdetail' element={<BlogDetail/>}/>
+      </Routes>
     </div>
   )
 }
