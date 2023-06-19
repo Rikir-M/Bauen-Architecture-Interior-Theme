d55ef5ddbf8c7b5a7e7ba4f8836415d2abab4b2b
EkhaingSoe <eikhaingsoe.ycc@gmail.com>
2023-06-20 01:04:56 +0630

    scrollbtnFinished

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import Navbar from './components/Navbar'
 import About from './pages/About'
 import Home from './pages/Home'
 import Services from './pages/Services'
+import ServiceDetail from './pages/ServiceDetail'
+import ScrollBtn from './components/ScrollBtn'
 
 const App = () => {
   return (
+  
     <div className='selection:bg-selectionColor'>
+      <h1 className='animate__animated animate__fadeInUp text-9xl text-white'>hello</h1>
       {/* <Navbar/>
       <Home/> */}
-      {/* <Services/> */}
-      <About/>
+      <Services/>
+      <ScrollBtn  />
+      {/* <About/> */}
       {/* <Footer/> */}
+      {/* <ServiceDetail/> */}
     </div>
   )
 }
